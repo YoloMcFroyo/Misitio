155d5198b1be0daed2d65164d257bd71c5948a54
Alan Talavera-Cordova <126989249+YoloMcFroyo@users.noreply.github.com>
2026-09-18 16:22:42 -0700

    Refactor App component to implement navigation with MapScreen and FeedScreen

--- App.tsx
+++ App.tsx
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import MapScreen from "./src/screens/MapScreen";
+import FeedScreen from "./src/screens/FeedScreen";
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="FeedScreen"
+        screenOptions={{
+          headerStyle: { backgroundColor: 'blue' },
+          headerTintColor: 'white',
+          headerTitleStyle: { fontWeight: 'bold' },
+        }}
+      >
+        <Stack.Screen
+          name="MapScreen"
+          component={MapScreen}
+        />
+        <Stack.Screen
+          name="FeedScreen"
+          component={FeedScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer >
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
