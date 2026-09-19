1e80a91d3e09c2249278bdd8acac85ff59a6de84
Alan Talavera-Cordova <126989249+YoloMcFroyo@users.noreply.github.com>
2026-09-18 17:44:28 -0700

    Refactor TabNavigator to include custom tab icons for Map and Feed screens.
    Add highlighting on icon for whichever tab is active.

--- src/navigation/TabNavigator.tsx
+++ src/navigation/TabNavigator.tsx
@@ -1,15 +1,70 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Image, ImageSourcePropType, View } from 'react-native';
 import MapScreen from '../screens/MapScreen';
 import FeedScreen from "../screens/FeedScreen";
 
+const Styles = {
+    tabBarIconActiveColor: '#00f7ff',
+    tabBarIconInactiveColor: 'white'
+};
 
 const Tab = createBottomTabNavigator();
 
+const TabIcon = ({ source, size, focused }: {
+    source: ImageSourcePropType;
+    size: number;
+    focused: boolean;
+}) => (
+    <View style={{
+        backgroundColor: focused ? 'rgba(255, 255, 255, 0.4)' : 'transparent',
+        borderRadius: 50,
+        padding: 10
+    }}>
+        <Image
+            source={source}
+            style={{ width: size, height: size }}
+            tintColor={focused ? Styles.tabBarIconActiveColor : Styles.tabBarIconInactiveColor}
+        />
+    </View>
+);
+
 export default function MyTabs() {
     return (
-        <Tab.Navigator>
-            <Tab.Screen name='MapScreen' component={MapScreen} />
-            <Tab.Screen name='FeedScreen' component={FeedScreen} />
+        <Tab.Navigator
+            screenOptions={{
+                tabBarStyle: { backgroundColor: '#5b5b5b' },
+                tabBarShowLabel: false,
+                tabBarIconStyle: {
+                    marginTop: 9
+                },
+            }}
+        >
+            <Tab.Screen
+                name='Map'
+                component={MapScreen}
+                options={{
+                    tabBarIcon: ({ size, focused }) => (
+                        <TabIcon
+                            source={require("../../assets/MapScreenIcon.png")}
+                            size={size}
+                            focused={focused}
+                        />
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name='Entries'
+                component={FeedScreen}
+                options={{
+                    tabBarIcon: ({ size, focused }) => (
+                        <TabIcon
+                            source={require("../../assets/FeedScreenIcon.png")}
+                            size={size}
+                            focused={focused}
+                        />
+                    ),
+                }}
+            />
         </Tab.Navigator>
     )
 }
